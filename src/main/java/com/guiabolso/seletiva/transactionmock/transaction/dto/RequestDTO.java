77bb9d6a9df31835352e6e41db3663183ab6ed88
victorhugopb8 <victorhugopb8@gmail.com>
2020-09-19 19:34:08 -0300

    Ajuste em DTO

--- src/main/java/com/guiabolso/seletiva/transactionmock/transaction/dto/RequestDTO.java
+++ src/main/java/com/guiabolso/seletiva/transactionmock/transaction/dto/RequestDTO.java
@@ -1,13 +1,12 @@
 package com.guiabolso.seletiva.transactionmock.transaction.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestDTO {
     @NonNull
     private Long userId;
